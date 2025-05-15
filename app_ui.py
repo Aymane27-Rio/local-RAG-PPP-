2eec022cd928e41e82977313d698ea4bfba2b692
Aymane <147632834+Aymane27-Rio@users.noreply.github.com>
2025-05-15 17:44:18 +0100

    Update app_ui.py

--- app_ui.py
+++ app_ui.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from app import LocalRAGApp  # your existing RAG class
+from app import LocalRAGApp #importing app
 
 st.set_page_config(page_title="Local RAG App", layout="centered")
 
@@ -8,7 +8,7 @@ st.markdown("Upload a PDF and ask questions about it!")
 
 app = LocalRAGApp()
 
-# Sidebar controls
+
 with st.sidebar:
     st.header("Step 1: Upload PDF")
     uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
@@ -20,10 +20,10 @@ with st.sidebar:
         app.setup_retrieval_chain()
         st.success("Document loaded and vector DB created!")
 
-# Main Q&A section
+
 if uploaded_file:
     question = st.text_input("Ask a question about your document:")
     if question:
         response = app.chain.invoke(question)
-        st.markdown("### 📥 Answer:")
+        st.markdown("### Answer:")
         st.success(response)
